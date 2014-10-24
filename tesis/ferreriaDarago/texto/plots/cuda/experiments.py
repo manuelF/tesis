9f7ee2186fd87ed1fc8eec284ed038ad24714360
manuelF <m.ferreria@gmail.com>
2014-10-24 18:40:55 -0300

    Agregados graficos y reescrito el threading

--- tesis/ferreriaDarago/texto/plots/cuda/experiments.py
+++ tesis/ferreriaDarago/texto/plots/cuda/experiments.py
@@ -31,7 +31,42 @@ def speedupDoble():
       'filename':"4placas-doble.png"}
   barGraph(**params)
 
+def threading():
+  ref_m2090 = 4019568.0
+  change_m2090 = 1156914.0
+  ref_kepler = 1.0
+  change_kepler = 0.5
+  measures = (ref_m2090/ref_m2090, ref_m2090/change_m2090,
+      ref_kepler/ref_kepler, ref_kepler/change_kepler)
+  print measures
+  params =  {'title': u"Speedup del computo de densidad electronica",
+      'xlabel':u"Arquitecturas GPU",
+      'ylabel':u"Aceleración (en veces)",
+      'values':measures,
+      'ticks':(u'Referencia Fermi', u'Optimizado Fermi',
+        u'Referencia Kepler', u'Optimizado Kepler'),
+      'filename':"threading.png"}
+  barGraph(**params)
+
+def speedupTotal():
+  ref_1x2090 = 5005751
+  change_iteration_1x2090 = 909456
+  change_iteration_4x2090 = 320102
+  measures = (ref_1x2090/ref_1x2090,
+      ref_1x2090/change_iteration_1x2090,
+      ref_1x2090/change_iteration_4x2090)
+  print measures
+  params =  {'title': u"Speedup del calculo de SCF aplicando todas las optimizaciones",
+      'xlabel':u"Arquitecturas GPU",
+      'ylabel':u"Aceleración (en veces)",
+      'values':measures,
+      'ticks':(u'Referencia Fermi', u'Optimizado Fermi 1 placa', u'Optimizado Fermi 4 placas'),
+      'filename':"final.png"}
+  barGraph(**params)
+
 if __name__ == '__main__':
+  threading()
   texture()
   speedupSimple()
   speedupDoble()
+  speedupTotal()
