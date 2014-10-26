f308128899295a1605c2397fde86d430edf5c965
manuelF <m.ferreria@gmail.com>
2014-10-26 13:48:33 -0300

    Agregados mas graficos, reescrito algo del global free

--- tesis/ferreriaDarago/texto/plots/cuda/experiments.py
+++ tesis/ferreriaDarago/texto/plots/cuda/experiments.py
@@ -39,7 +39,7 @@ def threading():
   measures = (ref_m2090/ref_m2090, ref_m2090/change_m2090,
       ref_kepler/ref_kepler, ref_kepler/change_kepler)
 
-  params =  {'title': u"Speedup del computo de densidad electronica",
+  params =  {'title': u"Speedup de density cambiando el threading",
       'xlabel':u"Arquitecturas GPU",
       'ylabel':u"Aceleración (en veces)",
       'values':measures,
@@ -60,7 +60,7 @@ def variandoDBS():
   measures = (m2090_32, m2090_64, m2090_128,
     k40_32, k40_64, k40_128)
 
-  params =  {'title': u"Speedup del computo de densidad electronica",
+  params =  {'title': u"Tiempo del computo de density variando el tamaño del bloque",
       'xlabel':u"Tamaño del bloque",
       'ylabel':u"Tiempo del kernel density [us]",
       'values':measures,
@@ -69,7 +69,49 @@ def variandoDBS():
       'filename':"dbs.png"}
   barGraph(**params)
 
+def shared4vs3():
+  m2090_shared3 = 638061.0
+  m2090_shared4 = 631758.0
 
+  k40_shared3 = 401664.0
+  k40_shared4 = 423654.0
+
+  measures = (m2090_shared4/m2090_shared3, k40_shared4/k40_shared3)
+
+  params =  {'title': u"Speedup del computo de density variando el tamaño de la estructuras",
+      'xlabel':u"Tamaño de los elementos en la shared",
+      'ylabel':u"Aceleracion en veces del kernel density [us]",
+      'values':measures,
+      'ticks':(u'Fermi float3 contra float4',
+        u'Kepler float3 contra float4',),
+      'filename':"shared-4vs3.png"}
+  barGraph(**params)
+
+def globalMemory():
+#iteration fullereno
+  m2090_gpu0 = 3241784.483871
+  m2090_gpu1 = 2775760.1935484
+  m2090_gpu2 = 2688267.2580645
+  m2090_gpu3 = 2745970.8709677
+  m2090_gpu4 = 2742110.2903226
+  m2090_gpu5 = 2710770.3870968
+  m2090_gpu6 = 2722721.9677419
+  m2090_gpu7 = 2653487.0
+  
+  measures = (m2090_gpu0/m2090_gpu0, 
+    m2090_gpu0/m2090_gpu1,m2090_gpu0/m2090_gpu2,
+    m2090_gpu0/m2090_gpu3,m2090_gpu0/m2090_gpu4,
+    m2090_gpu0/m2090_gpu5,m2090_gpu0/m2090_gpu6,
+    m2090_gpu0/m2090_gpu7)
+
+  params =  {'title': u"Speedup del computo de densidad electronica variando el tamaño del cacheo",
+      'xlabel':u"Tamaño de la memoria global disponible",
+      'ylabel':u"Aceleración (en veces)",
+      'values':measures,
+      'ticks':(u'0 Mb',u'530 Mb', u'1060 Mb',u'1590 Mb',
+		    u'2650 Mb',u'3180 Mb', u'3710 Mb',u'4240 Mb',),
+      'filename':"global-fullereno.png"}
+  barGraph(**params)
 
 def speedupTotal():
   ref_1x2090 = 5005751
@@ -94,3 +136,5 @@ if __name__ == '__main__':
   speedupDoble()
   speedupTotal()
   variandoDBS()
+  shared4vs3()
+  globalMemory()
