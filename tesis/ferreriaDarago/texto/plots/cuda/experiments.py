6e1cf18ebe96beb7c3f92dfae3c3b5e10893b26e
mferreria <mferreria@2258c766-92ac-47c0-ac32-b068d4181763>
2014-10-31 14:15:26 +0000

    Agregados graficos y mas formas de generarlos
    git-svn-id: svn+ssh://lsc.dc.uba.ar/var/lib/svn/chemhibri/@225 2258c766-92ac-47c0-ac32-b068d4181763

--- tesis/ferreriaDarago/texto/plots/cuda/experiments.py
+++ tesis/ferreriaDarago/texto/plots/cuda/experiments.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
+import numpy as np
 sys.path.insert(0, '../comun')
 from graphs import *
 
 def texture():
-  params =  {'title': u"Speedup de uso de memorias de textura",
+  params =  { #'title': u"Speedup de uso de memorias de textura",
       'xlabel':u"Arquitectura GPU",
       'ylabel':u"Aceleración (en veces)",
-      'values':(1.0,1.21,1.38),
+      'yvalues':(1.0,1.21,1.38),
+      'ylim':(0.0,2.0),
       'ticks':(u'Referencia', u'Fermi', u'Kepler'),
       'filename':"texture.png"}
   barGraph(**params)
@@ -17,7 +19,8 @@ def speedupSimple():
   params =  {'title': u"Speedup en multiples M2090 (simple precision)",
       'xlabel':u"Cantidad de placas en un mismo nodo",
       'ylabel':u"Aceleración (en veces)",
-      'values':(1.0,1.83,2.6,2.86),
+      'yvalues':(1.0,1.83,2.6,2.86),
+      'ylim':(0.0,4.0),
       'ticks':(u'1 placa', u'2 placas', u'3 placas', u'4 placas'),
       'filename':"4placas-simple.png"}
   barGraph(**params)
@@ -26,7 +29,8 @@ def speedupDoble():
   params =  {'title': u"Speedup en multiples M2090 (doble precision)",
       'xlabel':u"Cantidad de placas en un mismo nodo",
       'ylabel':u"Aceleración (en veces)",
-      'values':(1.0,2.03,2.92,3.8),
+      'yvalues':(1.0,2.03,2.92,3.8),
+      'ylim':(0.0,4.0),
       'ticks':(u'1 placa', u'2 placas', u'3 placas', u'4 placas'),
       'filename':"4placas-doble.png"}
   barGraph(**params)
@@ -36,15 +40,15 @@ def threading():
   change_m2090 = 3077739.0
   ref_kepler = 4358695.0
   change_kepler = 1929594.0
-  measures = (ref_m2090/ref_m2090, ref_m2090/change_m2090,
-      ref_kepler/ref_kepler, ref_kepler/change_kepler)
+  measures = (ref_m2090/change_m2090,
+      ref_kepler/change_kepler)
 
-  params =  {'title': u"Speedup de density cambiando el threading",
+  params =  {'title': u"Aceleracion de density cambiando el threading",
       'xlabel':u"Arquitecturas GPU",
       'ylabel':u"Aceleración (en veces)",
-      'values':measures,
-      'ticks':(u'Referencia Fermi', u'Optimizado Fermi',
-        u'Referencia Kepler', u'Optimizado Kepler'),
+      'yvalues':measures,
+      'ylim':(0.0,3.),
+      'ticks':(u'Aceleracion Fermi', u'Aceleracion Kepler'),
       'filename':"threading.png"}
   barGraph(**params)
 
@@ -56,16 +60,17 @@ def variandoDBS():
   k40_32 = 579138.0
   k40_64 = 401664.0
   k40_128 = 494509.0
-  
+
   measures = (m2090_32, m2090_64, m2090_128,
     k40_32, k40_64, k40_128)
 
   params =  {'title': u"Tiempo del computo de density variando el tamaño del bloque",
       'xlabel':u"Tamaño del bloque",
       'ylabel':u"Tiempo del kernel density [us]",
-      'values':measures,
-      'ticks':(u'Fermi DBS 32', u'Fermi DBS 64',u'Fermi DBS 128',
-         u'Kepler DBS 32', u'Kepler DBS 64',u'Keploer DBS 128',),
+      'yvalues':measures,
+      'ylim':(0.0,max(list(measures))*1.1),
+      'ticks':(u'Fermi 32', u'Fermi 64',u'Fermi 128',
+         u'Kepler 32', u'Kepler 64',u'Kepler 128',),
       'filename':"dbs.png"}
   barGraph(**params)
 
@@ -78,40 +83,103 @@ def shared4vs3():
 
   measures = (m2090_shared4/m2090_shared3, k40_shared4/k40_shared3)
 
-  params =  {'title': u"Speedup del computo de density variando el tamaño de la estructuras",
+  params =  {#'title': u"Speedup del computo de density variando el tamaño de la estructuras",
       'xlabel':u"Tamaño de los elementos en la shared",
       'ylabel':u"Aceleracion en veces del kernel density [us]",
-      'values':measures,
+      'yvalues':measures,
+      'ylim':(0.0,1.2),
       'ticks':(u'Fermi float3 contra float4',
         u'Kepler float3 contra float4',),
       'filename':"shared-4vs3.png"}
   barGraph(**params)
 
 def globalMemory():
-#iteration fullereno
-  m2090_gpu0 = 3241784.483871
-  m2090_gpu1 = 2775760.1935484
-  m2090_gpu2 = 2688267.2580645
-  m2090_gpu3 = 2745970.8709677
-  m2090_gpu4 = 2742110.2903226
-  m2090_gpu5 = 2710770.3870968
-  m2090_gpu6 = 2722721.9677419
-  m2090_gpu7 = 2653487.0
-  
-  measures = (m2090_gpu0/m2090_gpu0, 
-    m2090_gpu0/m2090_gpu1,m2090_gpu0/m2090_gpu2,
-    m2090_gpu0/m2090_gpu3,m2090_gpu0/m2090_gpu4,
-    m2090_gpu0/m2090_gpu5,m2090_gpu0/m2090_gpu6,
-    m2090_gpu0/m2090_gpu7)
+#iteration fullereno - giol - 2090 - master
+  m2090_gpu0 = 3358165.5555556
+  m2090_gpu1 = 2768049.2222222
+  m2090_gpu2 = 2824399.3333333
+  m2090_gpu3 = 2674648.2222222
+  m2090_gpu4 = 2630395.0
+  m2090_gpu5 = 2619868.3333333
+  m2090_gpu6 = 2690540.5555556
+  m2090_gpu7 = 2675686.2222222
 
-  params =  {'title': u"Speedup del computo de densidad electronica variando el tamaño del cacheo",
+  vals = [m2090_gpu0, m2090_gpu1, m2090_gpu2, m2090_gpu3,
+      m2090_gpu4, m2090_gpu5, m2090_gpu6, m2090_gpu7]
+  measures =  tuple( map((lambda x: (1/x) * (m2090_gpu0)), vals))
+
+  params =  {#'title': u"Speedup del computo de densidad electronica variando el tamaño del cacheo",
       'xlabel':u"Tamaño de la memoria global disponible",
       'ylabel':u"Aceleración (en veces)",
-      'values':measures,
+      'yvalues':measures,
+      'ylim':(0.8,1.4),
       'ticks':(u'0 Mb',u'530 Mb', u'1060 Mb',u'1590 Mb',
 		    u'2650 Mb',u'3180 Mb', u'3710 Mb',u'4240 Mb',),
       'filename':"global-fullereno.png"}
   barGraph(**params)
+
+def globalMemoryDetailed():
+#iteration fullereno - giol - 2090 - master
+  m2090_gpu0 = 3358165.5555556
+  m2090_gpu1e_5 = 3078160.6666667
+  m2090_gpu1e_4 = 2909544.8888889
+  m2090_gpu1e_3 = 2807708.6666667
+  m2090_gpu1e_2 = 2815344.3333333
+  m2090_gpu1e_1 = 2768049.2222222
+
+  vals = [m2090_gpu0, m2090_gpu1e_5, m2090_gpu1e_4, m2090_gpu1e_3,
+      m2090_gpu1e_2, m2090_gpu1e_1 ]
+  measures =  tuple( map((lambda x: (1/x) * (m2090_gpu0)), vals))
+
+
+  params =  {#'title': u"Aceleracion del computo de densidad electronica variando el tamaño del cacheo",
+      'xlabel':u"Tamaño de la memoria global disponible",
+      'ylabel':u"Aceleración (en veces)",
+      'yvalues':measures,
+      'ylim':(0.8,1.4),
+      'ticks':(u'0 Mb',u'0.053 Mb', u'0.53 Mb',u'5.3 Mb',
+		    u'53 Mb',u'530 Mb'),
+      'filename':"global-detailed-fullereno.png"}
+  barGraph(**params)
+
+def acumuladoGlobalMemory():
+#Acumulando los runtimes de funciones en Fullereno 2090 Giol
+  fileformat = {'names': ('index','size','rmm','density','functions'),
+        'formats' :('uint64','uint64','uint64','uint64','uint64')}
+  measures = np.loadtxt("measures/tiempos_frac/fullerenos/size_tiempos", fileformat)
+  ordered = np.sort(measures, order='size')
+  runtimes_partials = np.cumsum(np.divide((ordered['functions']),
+      np.float(np.sum(ordered['functions']))))
+
+  params =  {#'title': u"Aceleración del calculo de SCF aplicando todas las optimizaciones",
+      'xlabel':u"Tamaño de las matrices acumuladas [Gb]",
+      'ylabel':u"Fracción del tiempo acumulado",
+      'yvalues':runtimes_partials,
+      'xvalues':np.divide(np.cumsum(ordered['size']),1024.**3),
+      'ylim' : (0,1),
+      'xlim' : (0, 1e8),
+      'filename':"global-functions-acc.png"}
+  stackGraph(**params)
+
+def predictorSizeInGpu():
+#Runtime total por grupo vs sizeingpu en Fullereno 2090 Giol
+  fileformat = {'names': ('index','size','rmm','density','functions'),
+        'formats' :('uint64','uint64','uint64','uint64','uint64')}
+  measures = np.loadtxt("measures/tiempos_frac/fullerenos/size_tiempos", fileformat)
+  ordered = np.sort(measures, order='size')
+  a_total = ordered['rmm'] + ordered['density'] + ordered['functions']
+
+  params =  {#'title': u"Aceleración del calculo de SCF aplicando todas las optimizaciones",
+      'xlabel':u"Tamaño de las matrices del grupo [Mb]",
+      'ylabel':u"Runtime de un grupo [ms]",
+      'yvalues':np.divide(a_total,1000.),
+      'xvalues':np.divide(ordered['size'],1024.**2),
+      'ylim' : (1,130),
+      'xlim' : (0, 300),
+      'ylegend': 'Runtime de un grupo',
+      'fitlegend': 'Fit lineal',
+      'filename':"sizeingpu-predictor-fullereno.png"}
+  scatterGraphFitLineal(**params)
 
 def speedupTotal():
   ref_1x2090 = 5005751
@@ -120,14 +188,19 @@ def speedupTotal():
   measures = (ref_1x2090/ref_1x2090,
       ref_1x2090/change_iteration_1x2090,
       ref_1x2090/change_iteration_4x2090)
-  print measures
-  params =  {'title': u"Speedup del calculo de SCF aplicando todas las optimizaciones",
-      'xlabel':u"Arquitecturas GPU",
+  params =  {'title': u"Aceleración del calculo de SCF aplicando todas las optimizaciones",
+      'xlabel':u"",
       'ylabel':u"Aceleración (en veces)",
-      'values':measures,
+      'yvalues':measures,
+      'ylim':(0.0,16),
       'ticks':(u'Referencia Fermi', u'Optimizado Fermi 1 placa', u'Optimizado Fermi 4 placas'),
       'filename':"final.png"}
   barGraph(**params)
+
+def coalescienciaTranspose():
+  # Medir runtime antes y despues del transpose
+  pass
+
 
 if __name__ == '__main__':
   threading()
@@ -138,3 +211,6 @@ if __name__ == '__main__':
   variandoDBS()
   shared4vs3()
   globalMemory()
+  globalMemoryDetailed()
+  acumuladoGlobalMemory()
+  predictorSizeInGpu()
