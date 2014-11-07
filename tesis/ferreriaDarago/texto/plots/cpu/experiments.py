d678d41846482f6d1706ebe7887272a48debdd9e
Juan Pablo Darago <jpdarago@gmail.com>
2014-11-07 14:50:46 -0300

    Mas de implementacion CPU

--- tesis/ferreriaDarago/texto/plots/cpu/experiments.py
+++ tesis/ferreriaDarago/texto/plots/cpu/experiments.py
@@ -7,17 +7,18 @@ sys.path.insert(0, '../comun')
 from graphs import *
 
 def hemo_group_sizes_histogram():
-    allfunctions, allpoints = [], []
+    allfunctions, allpoints, allindexes = [], [], []
     with open("measures/tamanios-cubos-esferas.txt", "r") as rawdata:
         for line in rawdata.readlines():
-            _, functions, points, _, _ = line.strip().split(" ")
+            _, functions, points, _, _, indexes = line.strip().split(" ")
             allfunctions.append(functions)
             allpoints.append(points)
+            allindexes.append(indexes)
 
     params = {
-        'xlabel': u"Rango de funciones por grupo",
-        'ylabel': u"Número de grupos",
-        'title': "Histograma de la cantidad de funciones para cada grupo",
+        'xlabel': u"Funciones por grupo",
+        'ylabel': u"Fracción de los grupos",
+        'title': u'',
         'nbins': 30,
         'values': [int(f) for f in allfunctions],
         'filename': "histogram-functions-hemo.png"
@@ -25,15 +26,24 @@ def hemo_group_sizes_histogram():
     histogram(**params)
 
     params = {
-        'xlabel': u"Rango de puntos por grupo",
-        'ylabel': u"Número de grupos",
-        'title': "Histograma de la cantidad de puntos para cada grupo",
+        'xlabel': u"Puntos por grupo",
+        'ylabel': u"Fracción de los grupos",
+        'title': u'',
         'nbins': 30,
         'values': [int(p) for p in allpoints],
         'filename': "histogram-points-hemo.png"
     }
     histogram(**params)
 
+    params = {
+        'xlabel': u"Indices de fock por grupo",
+        'ylabel': u"Fracción de los grupos",
+        'title': u'',
+        'nbins': 30,
+        'values': [int(p) for p in allindexes],
+        'filename': "histogram-indexes-hemo.png"
+    }
+    histogram(**params)
 
 if __name__ == '__main__':
     hemo_group_sizes_histogram()
